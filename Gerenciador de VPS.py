6d59442e244f1ab6a0488f8f5635a82c9f00a83b
vempire-ghost <132581535+vempire-ghost@users.noreply.github.com>
2024-09-28 01:06:11 -0300

    beta76.5
    Criado linha pontilhada na altura das latências 100 e 200ms.
    Removido acesso a antiga função de ping e plotter, método a ser removido no futuro.

--- Gerenciador de VPS.py
+++ Gerenciador de VPS.py
@@ -385,9 +385,9 @@ class ButtonManager:
         menu_bar.add_cascade(label="Configurações", menu=config_menu)
         config_menu.add_command(label="Configurações do Gerenciador de VPS", command=self.open_omr_manager)
         config_menu.add_command(label="Configurações de Cores", command=self.open_color_config)
-        config_menu.add_command(label="Monitoramento de Latência", command=self.run_vps_vpn_pings_with_plot)
+        #config_menu.add_command(label="Monitoramento de Latência", command=self.run_vps_vpn_pings_with_plot)
         config_menu.add_command(label="Console com OMR VPN", command=self.open_ssh_terminal)
-        config_menu.add_command(label="MTR e Grafico", command=self.execute_mtr_and_plot)
+        config_menu.add_command(label="Monitor MTR com Grafico", command=self.execute_mtr_and_plot)
         config_menu.add_command(label="Ajuda", command=self.abrir_arquivo_ajuda)
         config_menu.add_command(label="Sobre", command=self.about)
 
@@ -943,10 +943,10 @@ class ButtonManager:
         self.footer_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
         # Adiciona o label de versão ao rodapé
-        self.version_label = tk.Label(self.footer_frame, text="Projeto Temer - ©VempirE_GhosT - Versão: beta 76.4", bg='lightgray', fg='black')
+        self.version_label = tk.Label(self.footer_frame, text="Projeto Temer - ©VempirE_GhosT - Versão: beta 76.5", bg='lightgray', fg='black')
         self.version_label.pack(side=tk.LEFT, padx=0, pady=0)
 
-# METODO PARA MTR E PLOT DE PING
+# METODO PARA MTR E GRAFICO DE CONEXÕES
     def execute_mtr_and_plot(self):
         """Executa o comando MTR via SSH para múltiplas interfaces em uma única janela com quadros separados."""
         
@@ -1008,6 +1008,10 @@ class ButtonManager:
             ax.set_ylabel("Latência (ms)")
             ax.set_ylim(0, 300)
             ax.legend(loc='upper right')
+
+            # Adiciona linhas horizontais pretas nas alturas de 100 e 200 ms, sem labels
+            ax.axhline(y=100, color='black', linestyle='--', linewidth=0.5)  # Linha para 100 ms, mais fina
+            ax.axhline(y=200, color='black', linestyle='--', linewidth=0.5)  # Linha para 200 ms, mais fina
 
             # Adiciona o gráfico à interface Tkinter
             canvas = FigureCanvasTkAgg(fig, master=frame)
@@ -5290,7 +5294,7 @@ class about:
         button_frame.pack_propagate(False)
 
         # Adicionando imagens aos textos
-        self.add_text_with_image(button_frame, "Versão: Beta 76 | 2024 - 2024", "icone1.png")
+        self.add_text_with_image(button_frame, "Versão: Beta 76.5 | 2024 - 2024", "icone1.png")
         self.add_text_with_image(button_frame, "Edição e criação: VempirE", "icone2.png")
         self.add_text_with_image(button_frame, "Código: Mano GPT com auxilio Fox Copilot", "icone3.png")
         self.add_text_with_image(button_frame, "Auxilio não remunerado: Mije", "pepox.png")
