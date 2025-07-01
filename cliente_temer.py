79f7d5a3970d059e34ab527b68e6ebee99eb1c06
vempire-ghost <132581535+vempire-ghost@users.noreply.github.com>
2025-06-30 23:38:30 -0300

    beta2.1 cliente_temer.py
    Teste para corrigir problema ao iniciar o programa depois de atualizado.

--- cliente_temer.py
+++ cliente_temer.py
@@ -34,7 +34,7 @@ if getattr(sys, 'frozen', False):
 
 # Função para retornar a versão
 def get_version():
-    return "Beta 2.0"
+    return "Beta 2.1"
 
 class ClientApp:
     def __init__(self):
@@ -189,7 +189,7 @@ class ClientApp:
     move /y "{temp_name}" "cliente_temer.exe" >nul
     echo [ATUALIZADOR] Iniciando nova versão...
     timeout /t 5 /nobreak >nul
-    start cliente_temer.exe
+    start "" "cliente_temer.exe"
     del update.bat
     exit
     """)
